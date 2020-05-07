ffc8aaea03514263d13ff3a3edef2fae71d1fb54
Kilian Steinberg <steinberk@gmx.de>
2020-05-07 02:36:57 +0200

    Adjustment Sliders

--- js/draw.js
+++ js/draw.js
@@ -2,6 +2,9 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var button = document.getElementById("download");
 
+var x_range = document.getElementById('slide-x');
+var y_range = document.getElementById('slide-y');
+
 var img = new Image();
 var overlay = new Image();
 overlay.src = "img/overlay.png";
@@ -13,8 +16,9 @@ document.getElementById('image-file').onchange = function(e) {
     img.src = URL.createObjectURL(this.files[0]);
     overlay.onload = draw;
     
-    
     button.style="visibility:visible";
+    x_range.style="visibility:visible";
+    y_range.style="visibility:visible";
   };
 
   button.addEventListener('click', function (e) {
@@ -23,26 +27,46 @@ document.getElementById('image-file').onchange = function(e) {
     button.href = dataURL;
   }); 
 
+  x_range.oninput = function(e){
+    console.log("x", x_range.value);
+    draw();
+  };
+
+  y_range.oninput = function(e){
+    console.log("y", y_range.value);
+    draw();
+  };
+
 
 function draw(){
+    //Clear Canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    //Get side length of overlay
     var sl = overlay.height;
+
+    //Get image properties
     var h = img.height;
     var w = img.width;
 
+    //Get slider value
+    var y = (parseInt(y_range.value)+50)/100;
+    var x = (parseInt(x_range.value)+50)/100;
+    console.log(x,y);
+
+    //Set Canvas Attributes
     canvas.width = sl;
     canvas.height = sl;
 
     //ctx.drawImage(img,img.height,img.height);
     
     if (h >= w)
-        {ctx.drawImage(img, 0, (h-w)/2, w, w, sl*0.15, sl*0.15, sl*0.7, sl*0.7)}
+        {ctx.drawImage(img, 0, (1-y)*(h-w)/2, w, w, x*sl*0.15, y*sl*0.15, sl*0.7, sl*0.7)}
     else 
-        {ctx.drawImage(img, (w-h)/2, 0, h, h, sl*0.15, sl*0.15, sl*0.7, sl*0.7)}
+        {ctx.drawImage(img, (1-x)*(w-h)/2, 0, h, h, x*sl*0.15, y*sl*0.15, sl*0.7, sl*0.7)}
     
         
     ctx.drawImage(overlay, 0, 0);
-
 }
 
 
