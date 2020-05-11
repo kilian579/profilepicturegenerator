e2ceb5014bf20cbd3a2862f7e02f77eb62b56c6d
Kilian Steinberg <steinberk@gmx.de>
2020-05-11 23:31:38 +0200

    Position Sliders and Different Choices

--- js/draw.js
+++ js/draw.js
@@ -1,15 +1,39 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+//Enter Paths to all Overlay images here
+var overlays = ["img/overlay.svg", "img/overlay_hat.svg"];
+
+var canvas_preview = document.getElementById('canvas');
+var ctx_preview = canvas_preview.getContext('2d');
 var button = document.getElementById("download");
 
 var x_range = document.getElementById('slide-x');
 var y_range = document.getElementById('slide-y');
 
+var canvas_original = document.createElement("canvas");
+var ctx_original = canvas_original.getContext('2d');
+
 var img = new Image();
 var overlay = new Image();
-overlay.src = "img/overlay.png";
+overlay.src = overlays[0];
+
+var div_choose = document.getElementById("choose-ov");
+
+for (const [k, path] of Object.entries(overlays)){
+  
+  var img_choice = new Image(100,100);
+  img_choice.src = path;
+  
+  img_choice.addEventListener('click', function (e) {
+    overlay.src = path;
+    draw();
+  }); 
+
+  div_choose.appendChild(img_choice);
+
+}
 
 
+//Only for Development:
+//document.body.appendChild(canvas_original);
 document.getElementById('image-file').onchange = function(e) {
     img.onload = draw;
     img.onerror = failed;
@@ -17,8 +41,7 @@ document.getElementById('image-file').onchange = function(e) {
     overlay.onload = draw;
     
     button.style="visibility:visible";
-    x_range.style="visibility:visible";
-    y_range.style="visibility:visible";
+    document.getElementById('slidecontainer').style = "visibility:visible";
   };
 
   button.addEventListener('click', function (e) {
@@ -28,45 +51,58 @@ document.getElementById('image-file').onchange = function(e) {
   }); 
 
   x_range.oninput = function(e){
-    console.log("x", x_range.value);
+    //console.log("x", x_range.value);
     draw();
   };
 
   y_range.oninput = function(e){
-    console.log("y", y_range.value);
     draw();
   };
 
-
+  
+//Call the draw() function in your html body onload
 function draw(){
     //Clear Canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    //Get side length of overlay
-    var sl = overlay.height;
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     //Get image properties
     var h = img.height;
     var w = img.width;
 
+    //Get side length of overlay
+    var sl_original;
+    if (h >= w) {sl_original = w;} else {sl_original = h;}
+    var sl_preview = 500;
+
     //Get slider value
-    var y = (parseInt(y_range.value)+50)/100;
-    var x = (parseInt(x_range.value)+50)/100;
+    var y = (parseInt(y_range.value)-50)/100;
+    var x = (parseInt(x_range.value)-50)/100;
     console.log(x,y);
 
-    //Set Canvas Attributes
-    canvas.width = sl;
-    canvas.height = sl;
+    //Setup both canvases
+    canvas_preview.width = sl_preview;
+    canvas_preview.height = sl_preview;
 
-    //ctx.drawImage(img,img.height,img.height);
-    
-    if (h >= w)
-        {ctx.drawImage(img, 0, (1-y)*(h-w)/2, w, w, x*sl*0.15, y*sl*0.15, sl*0.7, sl*0.7)}
-    else 
-        {ctx.drawImage(img, (1-x)*(w-h)/2, 0, h, h, x*sl*0.15, y*sl*0.15, sl*0.7, sl*0.7)}
-    
-        
-    ctx.drawImage(overlay, 0, 0);
+    canvas_original.width = sl_original;
+    canvas_original.height = sl_original;
+
+    //Draw on both canvases
+    const canvi = {[sl_preview]:ctx_preview, [sl_original]:ctx_original};
+
+    for (const [sl, ctx] of Object.entries(canvi)){
+      console.log(sl, ctx);
+      //(h-w)/2
+      //(w-h)/2
+      if (h >= w)
+          {ctx.drawImage(img, 0,0, w, h, 
+            x*sl+0.15*sl, y*sl, sl*0.7, (h/w)*sl*0.7);
+          }
+      else 
+          {ctx.drawImage(img, 0, 0, h, h, 
+            x*sl, y*sl+0.15*sl, (w/h)*sl*0.7, sl*0.7)}
+
+      ctx.drawImage(overlay, 0, 0, sl, sl);
+    }
 }
 
 
